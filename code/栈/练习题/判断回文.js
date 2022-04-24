c6fc5ef1df463c282b296ac13ad648e82898db32
黄建宇 <565166310@qq.com>
2022-04-24 22:19:28 +0800

    style: 格式化

--- code/栈/练习题/判断回文.js
+++ code/栈/练习题/判断回文.js
@@ -18,5 +18,5 @@ function isPalindrome(word) {
         reversedWord += stack.pop();
     }
 
-    return word === reversedWord
+    return word === reversedWord;
 }
--- code/栈/练习题/数制间的相互转化.js
+++ code/栈/练习题/数制间的相互转化.js
@@ -4,7 +4,7 @@ const Stack = require('../index');
  * @description 利用栈将一个数字从一种数制转换成另一种数制
  * @param {number} num 需要被转换的值
  * @param {number} base 数制/进制
- * @returns 转换过来的值
+ * @returns {string} 转换过来的值
  */
 function mulBase(num, base) {
     const stack = new Stack();
