15761765d859e2cdbb9e58d5ddd675a3198b6b45
黄建宇 <565166310@qq.com>
2022-04-24 22:01:09 +0800

    docs: 补充栈结构的注释

--- code/栈/index.js
+++ code/栈/index.js
@@ -2,26 +2,32 @@ function Stack() {
     this.items = [];
 }
 
+/** 入栈 */
 Stack.prototype.push = function (element) {
     this.items.push(element);
 }
 
+/** 出栈，并返回栈顶元素 */
 Stack.prototype.pop = function () {
     return this.items.pop();
 }
 
+/** 查看栈顶元素 */
 Stack.prototype.peek = function () {
     return this.items[this.items.length - 1];
 }
 
+/** 检查栈是否为空 */
 Stack.prototype.isEmpty = function () {
     return this.items.length === 0;
 }
 
+/** 查看栈的长度 */
 Stack.prototype.size = function () {
     return this.items.length;
 }
 
+/** 输出格式化内容 */
 Stack.prototype.toString = function () {
     let result = '';
     for (let item of this.items) {
@@ -29,9 +35,5 @@ Stack.prototype.toString = function () {
     }
     return result;
 }
-
-
-// 实例化
-const stack = new Stack();
 
 module.exports = Stack;
