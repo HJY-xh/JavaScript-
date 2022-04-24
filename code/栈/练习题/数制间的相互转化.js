1ba50631fa43dc9688d601d686e0e7e709f587e5
黄建宇 <565166310@qq.com>
2022-04-24 22:08:01 +0800

    docs: 习题注释修改

--- code/栈/练习题/数制间的相互转化.js
+++ code/栈/练习题/数制间的相互转化.js
@@ -1,15 +1,10 @@
 const Stack = require('../index');
 
 /**
- * 可以利用栈将一个数字从一种数制转换成另一种数制
- */
-
-
-/**
- * 
- * @param {需要被转换的值} num 
- * @param {数制/进制} base 
- * @returns {转换过来的值}
+ * @description 利用栈将一个数字从一种数制转换成另一种数制
+ * @param {number} num 需要被转换的值
+ * @param {number} base 数制/进制
+ * @returns 转换过来的值
  */
 function mulBase(num, base) {
     const stack = new Stack();
