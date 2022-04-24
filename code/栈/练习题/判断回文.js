3798b1b1def7fe95ed1cc9f2cc9f6df7e7a3c0f3
黄建宇 <565166310@qq.com>
2022-04-24 22:14:17 +0800

    习题:判断给定字符串是否为回文

---
+++ code/栈/练习题/判断回文.js
@@ -0,0 +1,22 @@
+const Stack = require('../index');
+
+/**
+ * @description 判断给定字符串是否为回文
+ * @param {string} word 给定的字符串˜
+ * @returns {boolean}
+ */
+function isPalindrome(word) {
+    if (word.length === 0) return false;
+
+    const stack = new Stack();
+    for (let item of word) {
+        stack.push(item);
+    }
+
+    let reversedWord = ''
+    while (stack.size() > 0) {
+        reversedWord += stack.pop();
+    }
+
+    return word === reversedWord
+}
